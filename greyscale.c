24125d6355d5e9e1d8837d0d2e491429aab2d895
Benjamin Sarol <sarolbenjamin_08221999@yahoo.com>
2022-03-04 15:53:56 -0600

    added single threaded grey scale

---
+++ greyscale.c
@@ -0,0 +1,104 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+
+// code from class
+// Cleans up and exits
+void cleanupAndClose(int exitCode);
+
+// Load a given image
+SDL_Surface* loadImage(char *filename);
+
+// shift red -> green -> blue -> red in the given image
+void shiftImage(SDL_Surface *image);
+
+inline void checkSDL(void* result)
+{
+    if(result == NULL)
+    {
+        fprintf(stderr, "Error: %s\n", SDL_GetError());
+        cleanupAndClose(EXIT_FAILURE);
+    }
+}
+
+SDL_Surface *source;
+
+int main(int argc, char *argv[])
+{
+    // Check that we have the right number of args.
+    if(argc != 2)
+    {
+        fprintf(stderr, "Usage: %s source \n", argv[0]);
+        cleanupAndClose(EXIT_FAILURE);
+    }
+
+    // We not do need to initialize SDL to load image data so we get right to
+    // loading the source image.
+    checkSDL(source = loadImage(argv[1]));
+
+    printf("grey..ing?\n");
+    shiftImage(source);
+
+	// Save image
+    printf("Saving: onegreyimage.png\n");
+    IMG_SavePNG(source, "onegreyimage.png");
+    // I have heard that the PNG writer is buggy... I have not had any trouble, but you
+    // can also use the BMP writer (hard to mess up BMPs).
+    //SDL_SaveBMP(source, "shiftOut.bmp");
+
+    cleanupAndClose(EXIT_SUCCESS);
+    return EXIT_SUCCESS;
+}
+
+void cleanupAndClose(int exitCode)
+{
+    if(source) SDL_FreeSurface(source);
+    exit(exitCode);
+}
+
+SDL_Surface* loadImage(char *filename)
+{
+    printf("Loading: %s\n", filename);
+
+    // Load the image into memory
+    SDL_Surface *image = IMG_Load(filename);
+
+    // Make sure pixel values are 32-bit
+    if(image)
+    {
+    	SDL_Surface *temp = SDL_ConvertSurfaceFormat(image, SDL_PIXELFORMAT_RGBA32, 0);
+    	SDL_FreeSurface(image);
+    	image = temp;
+    }
+
+    return image;
+}
+
+
+void greyimage(SDL_Surface *image)
+{
+	Uint32 *pixels = (Uint32 *)image->pixels;
+    for(int y = 0; y < image->h; y++)
+    {
+        for(int x = 0; x < image->w; x++)
+        {
+        	Uint32 pixel = pixels[(y * image->w) + x];
+
+        	Uint32 r = pixel & image->format->Rmask; // Isolate red component
+
+        	Uint32 g = pixel & image->format->Gmask; // Isolate green component
+
+        	Uint32 b = pixel & image->format->Bmask; // Isolate blue component
+
+
+
+			// Build the shifted pixel
+			pixels[(y * image->w) + x] = (b * 0.07) |
+										 (r * 0.21) |
+										 (g * 0.71) |
+										 (a);
+        }
+    }
+}
